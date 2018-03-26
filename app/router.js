f070dabbc7df0e606dbdd4ba83bb75a1cb04d699
OdearOgy <hovhan198@gmail.com>
2018-03-26 16:31:59 +0400

    Update project

--- app/router.js
+++ app/router.js
@@ -6,7 +6,12 @@ async function home_page(ctx) {
     await ctx.render('home_page/index');
 }
 
+async function home_page(ctx) {
+    await ctx.render('second_page/secondPage');
+}
+
 router.get('/home_page', home_page);
+router.get('/second_page', home_page);
 router.get('/', home_page);
 
 module.exports = router;
