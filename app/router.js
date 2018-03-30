cba327adb9852fbebbf73840d432fd6a68c62cf4
OdearOgy <hovhan198@gmail.com>
2018-03-30 09:11:31 +0400

    Update Pages

--- app/router.js
+++ app/router.js
@@ -6,12 +6,18 @@ async function home_page(ctx) {
     await ctx.render('home_page/index');
 }
 
-async function home_page(ctx) {
+async function second_page(ctx) {
     await ctx.render('second_page/secondPage');
 }
 
+async function new_second_page(ctx) {
+    await ctx.render('second_page/index');
+}
+
+
 router.get('/home_page', home_page);
-router.get('/second_page', home_page);
+router.get('/second_page', second_page);
+router.get('/new_second_page', new_second_page);
 router.get('/', home_page);
 
 module.exports = router;
---
+++ public/js/second_page/second_page.js
@@ -0,0 +1,43 @@
+
+//scroll animation
+function getElementY(query) {
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+}
+
+function doScrolling(element, duration) {
+	let startingY = window.pageYOffset
+  let elementY = getElementY(element)
+  let targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+	let diff = targetY - startingY
+  let easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  let start
+
+  if (!diff) return
+	window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    let time = timestamp - start
+    let percent = Math.min(time / duration, 1)
+    percent = easing(percent)
+
+    window.scrollTo(0, startingY + diff * percent)
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
+
+document.getElementById('scrolling').addEventListener('click', doScrolling.bind(null, '#section-id', 1000));
+
+
+
+//slider
+// var swiper_rev = new Swiper('.shape_abaj', {
+//     slidesPerView: 1,
+//     autoplay: 3000,
+//     speed: 800,
+//     spaceBetween: 8,
+//     paginationClickable: true,
+//     nextButton: '.swiper-button-next',
+//     prevButton: '.swiper-button-prev'
+//
+// });
