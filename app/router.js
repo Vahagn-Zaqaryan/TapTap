9fcdf0665c6fde89aa963df30dc6af9a19cf7e40
Vahagn Zaqaryan <vahagn.zaqaryan@tumo.org>
2018-03-30 17:24:03 +0400

    Router /home &  /details

--- app/router.js
+++ app/router.js
@@ -2,21 +2,19 @@
 
 const router = require('koa-router')();
 
-async function home_page(ctx) {
+async function home(ctx) {
     await ctx.render('home_page/index');
 }
 
-async function second_page(ctx) {
-    await ctx.render('second_page/secondPage');
-}
-
-async function new_second_page(ctx) {
+async function tour_details(ctx) {
     await ctx.render('second_page/index');
 }
 
-router.get('/home_page', home_page);
-router.get('/second_page', second_page);
-router.get('/new_second_page', new_second_page);
-router.get('/', home_page);
+router.get('/home_page', home);
+router.get('/home', home);
+router.get('/details', tour_details);
+router.get('/tourDetails', tour_details);
+router.get('/', tour_details);
+
 
 module.exports = router;
