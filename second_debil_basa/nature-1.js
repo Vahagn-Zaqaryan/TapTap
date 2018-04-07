98ec0bdea165ff6d284a1ac85764ddbab9fe3d1f
OdearOgy <hovhan198@gmail.com>
2018-04-08 01:10:33 +0400

    Add final changes

--- second_debil_basa/nature-1.js
+++ second_debil_basa/nature-1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const basa = {
-  bgImg: `14`, /*https://www.airbnb.com/experiences/23266?source=p2&currentTab=experience_tab&searchId=9afcf163-eb86-436e-bae6-ecd9ab8d3daf&federatedSearchId=67025189-54c2-4ba4-a47c-d4edbb65d32c&sectionId=374e3f35-1139-4c16-b3da-cb53ce007d11
+  bgImg: `30`, /*https://www.airbnb.com/experiences/23266?source=p2&currentTab=experience_tab&searchId=9afcf163-eb86-436e-bae6-ecd9ab8d3daf&federatedSearchId=67025189-54c2-4ba4-a47c-d4edbb65d32c&sectionId=374e3f35-1139-4c16-b3da-cb53ce007d11
       link֊երը փոխում եմ ի դեպ, ամեն մեկին իրա լինքն ա դրած եսլիշտօ
       link for photos, նկարները դու կքաշես մնացածը ես կդնեմ հեսա */
   name: `Off The Beaten Path Hiking in Madrid!`,
@@ -45,6 +45,7 @@ const basa = {
   },
 
   map: {
+    vand: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24183.872746070538!2d-4.058422754410818!3d40.74037553300141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd416d68f82b1fb9%3A0x3627523ad3ee7fd9!2sCercedilla%2C+Madrid%2C+Spain!5e0!3m2!1sen!2s!4v1523131252250`
     //ստեղ արդեն չգիտեմ ինչ գրեմ, Արտոն ասում էր մենակ Հայաստանն հելնի հերիք ա, սօ ստեղ մէփն ա էդ կետ մետն հետո,
     // էս չեմ հասկանում, ոնց կարա ինքը փոխի էլի, հասկանում եմ ուղղակի էդ շատ ա
     //լավ էս տեսնվելուց կխոսանք, թող մէփը ուղղակի քցի տեղը
